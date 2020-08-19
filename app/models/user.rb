7a3a40aacd53375908123582d56687cf907d0a94
beckyphan <github email address>
2020-08-18 22:22:28 -0700

    updated user validations to require presence of all fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, :email, :carb_grams, :protein_grams, :fat_grams, presence: :true
   validates :email, uniqueness: { case_sensitive: false}
   has_many :logs
 end
