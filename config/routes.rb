5a361ffc4d4a3be2cfee7a5c56d5185fcd73dee3
beckyphan <github email address>
2020-08-18 22:33:48 -0700

    created namespaced routes for users, logs, logged_foods, foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :show]
+      resources :logs, only: [:index, :create, :show, :edit]
+      resources :logged_foods, only: [:index, :create, :edit, :destroy]
+      resources :foods, only: [:index, :create, :edit, :destroy]
+    end
+  end
+  
 end
