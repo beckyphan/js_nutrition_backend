a0d90b6d7ca8943655bb6201c3a93b4093184ef2
beckyphan <github email address>
2020-08-21 23:21:13 -0700

    corrected user attributes, users#create logs in or registers a user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,30 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.find_by(email: user_params[:email])
+
+    if user && user.authenticate(user_params[:password])
+      render json: {user: UserSerializer.new(user)}, status: 200
+    elsif !user
+      new_user = User.new(user_params)
+      if new_user.valid?
+        render json: {user: UserSerializer.new(new_user)}, status: 200
+      else
+        render json: {error: 'failed to register user'}, status: 400
+      end
+    else
+      render json: {error: 'failed to login user'}, status: 400
+    end
   end
 
   def show
+  end
+
+  private
+  def login_params
+    params.require.(:user).permit(:email, :password)
+  end
+
+  def user_params
+    params.require.(:user).permit(:name, :email, :password, :carb_grams, :protein_grams, :fat_grams)
   end
 end
