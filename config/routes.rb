f323893eb19dd8fc07f85b6557fb353afd0c1696
beckyphan <github email address>
2020-08-21 22:50:01 -0700

    cleaned up routes, removed duplicates

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  get 'logs/index'
-  get 'logs/create'
-  get 'logs/show'
-  get 'logs/edit'
-  namespace :api do
-    namespace :v1 do
-      get 'users/create'
-      get 'users/show'
-    end
-  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
@@ -19,5 +10,5 @@ Rails.application.routes.draw do
       resources :foods, only: [:index, :create, :edit, :destroy]
     end
   end
-  
+
 end
