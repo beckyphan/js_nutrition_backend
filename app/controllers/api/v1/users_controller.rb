275078963cf875502d7dc97d79cd27bfaefbac1b
beckyphan <github email address>
2020-08-22 17:11:32 -0700

    changed debugger to pry, corrected private methods in user, tested user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.find_by(email: user_params[:email])
+    @user = User.find_by(email: user_params[:email])
 
-    if user && user.authenticate(user_params[:password])
-      render json: {user: UserSerializer.new(user)}, status: 200
-    elsif !user
-      new_user = User.new(user_params)
-      if new_user.valid?
-        render json: {user: UserSerializer.new(new_user)}, status: 200
+    if @user && @user.authenticate(user_params[:password])
+      render json: {user: UserSerializer.new(@user)}, status: 200
+    elsif !@user
+      @new_user = User.new(user_params)
+      if @new_user.valid?
+        @new_user.save!
+        render json: {user: UserSerializer.new(@new_user)}, status: 200
       else
         render json: {error: 'failed to register user'}, status: 400
       end
@@ -17,14 +18,16 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
+    user = User.find_by(id: params[:id])
+    render json: {user: UserSerializer.new(user)}, status: 200
   end
 
   private
   def login_params
-    params.require.(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password)
   end
 
   def user_params
-    params.require.(:user).permit(:name, :email, :password, :carb_grams, :protein_grams, :fat_grams)
+    params.require(:user).permit(:name, :email, :password, :carb_grams, :protein_grams, :fat_grams)
   end
 end
