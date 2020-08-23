e7af02e600f12f1782df48c597870bd610ebf17d
beckyphan <github email address>
2020-08-22 20:22:58 -0700

    generated food model and migration

---
+++ db/migrate/20200823030210_create_foods.rb
@@ -0,0 +1,14 @@
+class CreateFoods < ActiveRecord::Migration[6.0]
+  def change
+    create_table :foods do |t|
+      t.string :name
+      t.integer :quantity
+      t.string :unit
+      t.integer :carb
+      t.integer :protein
+      t.integer :fat
+
+      t.timestamps
+    end
+  end
+end
