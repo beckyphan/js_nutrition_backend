2869fee7c4e34398993341dd200aa55b0ddbdb67
beckyphan <github email address>
2020-08-22 20:28:27 -0700

    updated food model validations and association to log_foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  validates :name, uniqueness: { case_sensitive: false}
+  validates :quantity, :unit, :carb, :protein, :fat, presence: :true
+  has_many :log_foods
 end
