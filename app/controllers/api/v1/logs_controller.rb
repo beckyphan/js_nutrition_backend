44857c898e5771357367925a1acc8aecac5d927b
beckyphan <github email address>
2020-08-24 22:32:13 -0700

    created new log create action with nested route to capture belongs_to user info

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -5,11 +5,23 @@ class Api::V1::LogsController < ApplicationController
   end
 
   def create
+    @log = Log.new(log_param)
+    @log.user_id = params[:user_id]
+    if @log.save!
+      render json: LogSerializer.new(@log), status: 200
+    else
+      render json: {message: 'log must have unique date'}, status: 400
+    end
   end
 
   def show
   end
 
   def update
+  end
+
+  private
+  def log_param
+    params.require(:log).permit(:caldate)
   end
 end
