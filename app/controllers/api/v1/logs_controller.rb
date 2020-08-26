4a960a58e40cf1eef4d28ebf16167ed80b46379f
beckyphan <github email address>
2020-08-25 23:04:10 -0700

    updated Log#create where if existing date, then redirect to log#show

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -7,14 +7,18 @@ class Api::V1::LogsController < ApplicationController
   def create
     @log = Log.new(log_param)
     @log.user_id = params[:user_id]
-    if @log.save!
+    if @log.valid?
+      @log.save
       render json: LogSerializer.new(@log), status: 200
     else
-      render json: {message: 'log must have unique date'}, status: 400
+      @log = Log.find_by(log_param)
+      redirect_to action: "show", id: @log.id
     end
   end
 
   def show
+    @log = Log.find_by_id(params[:id])
+    render json: LogSerializer.new(@log), status: 200
   end
 
   def update
