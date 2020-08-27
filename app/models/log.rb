df60ea9ae1af9aeb9b7c790310485bb4489d2f75
beckyphan <github email address>
2020-08-26 21:44:40 -0700

    added custom methods to sum nutrient values of foods that belong to a log

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,4 +3,23 @@ class Log < ApplicationRecord
   validates :caldate, :uniqueness => true
   has_many :log_foods
   has_many :foods, through: :log_foods
+
+  # create methods to sum up nutrition?
+  def totalCarb
+    total = 0;
+    self.foods.each {|food| total += food.carb}
+    return total
+  end
+
+  def totalProtein
+    total = 0;
+    self.foods.each {|food| total += food.protein}
+    return total
+  end
+
+  def totalFat
+    total = 0;
+    self.foods.each {|food| total += food.fat}
+    return total
+  end
 end
